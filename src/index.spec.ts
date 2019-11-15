86abd892ee84cfa4bc160a5952dbebfd2a7166df
Maksim Skavysh <maksim.skavysh@gmail.com>
2019-11-16 01:18:24 +0300

    Add tests for runtimes

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,94 @@
-import { checkStoreConsistency } from './index'
+import {
+    stateConsistencyMiddleware,
+    registerStoreConsistencyValidator,
+    deleteStoreConsistencyValidator,
+} from './index'
+import { createStore, combineReducers, applyMiddleware } from "redux"
+import get = Reflect.get
 
-test('adds 1 + 2 to equal 3', () => {
-    const a: number = 1
-    expect(a + 2).toBe(3);
-});
+const ADD_TODO = 'ADD_TODO'
+const FAKE_ACTION = 'FAKE_ACTION'
+const DELETE_TODO_BROKEN = 'DELETE_TODO_BROKEN'
+
+const byId = (state = {}, action) => {
+    const { type, ...rest } = action
+    switch (type) {
+        case ADD_TODO:
+            return {
+                ...state,
+                [rest.id]: rest,
+            }
+        case DELETE_TODO_BROKEN:
+            return state // there error
+        case FAKE_ACTION:
+            return state // there error
+        default:
+            return state
+    }
+}
+
+const order = (state = [], action) => {
+    const { type, id } = action
+    switch (type) {
+        case ADD_TODO:
+            return state.concat(id)
+        case DELETE_TODO_BROKEN:
+            return state.filter(item => item !== id)
+        case FAKE_ACTION:
+            return state
+        default:
+            return state
+    }
+}
+
+const store = createStore(
+    combineReducers({ byId, order }),
+    {},
+    applyMiddleware(stateConsistencyMiddleware())
+)
+
+const idsConsistency = (state, action) => {
+    const { byId, order } = state
+    return order.every(id => !!byId[id])
+        || `Broken state consistency for ${action.type}`
+}
+
+const { dispatch, getState } = store
+
+const item1 = { text: 'Read the docs', id: 1 }
+const item2 = { text: 'dogs', id: 2 }
+
+test('Test initial store value', () => {
+    expect(store.getState()).toEqual({
+        byId: {},
+        order: [],
+    })
+})
+
+
+test('Test positive store validation scenarios', () => {
+    const mockedValidator = jest.fn((state, action) => true)
+    const validatorId = registerStoreConsistencyValidator(mockedValidator)
+    console.log('validatorId', validatorId)
+    const action1 = { type: ADD_TODO, ...item1 }
+    const action2 = { type: ADD_TODO, ...item2 }
+    dispatch(action1)
+    const state1 = getState()
+    dispatch(action2)
+    const state2 = getState()
+    expect(mockedValidator.mock.calls.length).toBe(2);
+    expect(mockedValidator.mock.calls[0][0]).toBe(state1)
+    expect(mockedValidator.mock.calls[1][0]).toBe(state2)
+
+    expect(getState()).toEqual({
+        byId: { [item1.id]: item1, [item2.id]: item2 },
+        order: [ item1.id, item2.id ],
+    })
+
+    dispatch({ type: FAKE_ACTION })
+    expect(mockedValidator.mock.calls.length).toBe(3);
+    deleteStoreConsistencyValidator(validatorId)
+    dispatch({ type: FAKE_ACTION })
+    expect(mockedValidator.mock.calls.length).toBe(3); // should not be changed
+})
+
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
 import * as t from 'runtypes'
 
+const LEVELS = { error: 'error', throw: 'throw' }
+
 type checkFn = (state: any, action: object) => void
+const levelsType = t.Union(t.Literal(LEVELS.error), t.Literal(LEVELS.throw))
 
-const checkFunctions: checkFn[] = []
+const checkFunctions = new Map()
+let lastId = (new Date()).valueOf()
 
-export const checkStoreConsistency = (checkers: checkFn[]) => {
-    checkers = t.Array(t.Function).check(checkers)
-    checkFunctions.push(...checkers)
+export const registerStoreConsistencyValidator = (checker: checkFn) => {
+    checker = t.Function.check(checker)
+    lastId = lastId + 1
+    checkFunctions.set(lastId, checker)
+    return lastId
 }
 
-const checkStateConsistency : checkFn = (state, action) => {
-    const errors = checkFunctions.map((f) => f(state, action))
+export const deleteStoreConsistencyValidator = (validatorId : string | number) => {
+    return checkFunctions.delete(validatorId)
 }
 
-export const stateConsistencyMiddleware = (params: { debounce: number }) => {
+const checkStateConsistencyCreator = ({ level = LEVELS.error } = {}): checkFn => (state, action) => {
+    levelsType.check(level)
+    const errors = Array.from(checkFunctions.values()).map((f) => f(state, action))
+}
+
+export const stateConsistencyMiddleware = ({ debounce = 0, level = LEVELS.error } = {}) => {
+    const checkStateConsistency = checkStateConsistencyCreator({ level })
     return ({getState}) => next => action => {
         const result = next(action)
         const state = getState()
